00fac588df27b034c715e6aa4586c8e42f3f57af
Dusan Ugarkovic <dusan.ugarkovic@instana.com>
2018-12-06 14:40:12 +0100

    changed handling response from promotion service

--- catalogue/server.js
+++ catalogue/server.js
@@ -158,7 +158,7 @@ function getPromotion(sku) {
 		    console('getPromotion promise response: ' + response.statusCode);
 			reject(error);
 		  } else {
-		    resolve(JSON.parse(body));
+		    resolve(body);
 		  }
 		});
 	})
